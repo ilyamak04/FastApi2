a3939b2f36268da6375a3101d86eb8faa84e4b87
ilyamak04 <makilya04@gmail.com>
2024-06-03 18:09:04 +0300

    configurate alembic

--- alembic/env.py
+++ alembic/env.py
@@ -18,14 +18,17 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from src.models import Base
+from src.config import settings
+
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", settings.db_url)
 
 
 def run_migrations_offline() -> None:
