ae63e632736f542204fac479e916c7bfdd5ceb49
ilyamak04 <makilya04@gmail.com>
2024-06-04 00:41:35 +0300

    add a few new model

--- src/users/models.py
+++ src/users/models.py
@@ -1,10 +1,41 @@
 from src.models import Base
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, Text, mapped_column, relationship, String
+from src.users.mixins import UserRelationMixin
 
 
 class Product(Base):
-    __tablename__ = "product"
 
     name: Mapped[str]
     price: Mapped[int]
     description: Mapped[str]
+
+
+class Post(UserRelationMixin, Base):
+    _user_id_nullable = False
+    _user_id_unique = False
+    _user_back_populates = "posts"
+
+    title: Mapped[str] = mapped_column(String(100), unique=False)
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        default_factory="",
+    )
+
+
+class User(Base):
+
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None]
