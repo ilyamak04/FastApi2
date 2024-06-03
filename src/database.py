60d03e851762856d0afc374c4429010833a66c2f
ilyamak04 <makilya04@gmail.com>
2024-06-03 16:48:18 +0300

    add scope session

--- src/database.py
+++ src/database.py
@@ -1,4 +1,10 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
 from src.config import settings
 
 
@@ -15,6 +21,22 @@ class DatabaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.sessions_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.sessions_factory() as session:
+            yield session
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.remove()
 
 
 db_helper = DatabaseHelper(
