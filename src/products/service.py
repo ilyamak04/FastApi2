688fecceba413c68aac06deed2c27c4a666f4528
ilyamak04 <makilya04@gmail.com>
2024-06-03 16:49:32 +0300

    logic for new endpoint

--- src/products/service.py
+++ src/products/service.py
@@ -2,14 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.users.models import Product
 from sqlalchemy import select
 from sqlalchemy.engine import Result
-from src.products.schemas import ProductCreate
+from src.products.schemas import ProductCreate, ProductUpdate, ProductUpdatePartial
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
     query = select(Product).order_by(Product.id)
     result: Result = await session.execute(query)
     products = result.scalars().all()
-    return list(products)
+    return products
 
 
 async def get_product(session: AsyncSession, product_id: int) -> Product | None:
@@ -24,3 +24,23 @@ async def create_product(
     await session.commit()
     await session.refresh()
     return product
+
+
+async def update_product(
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate | ProductUpdatePartial,
+    partial: bool = False,
+) -> Product:
+    for name, value in product_update.model_dump(exclude_unset=partial).items():
+        setattr(product, name, value)
+    await session.commit()
+    return product
+
+
+async def delete_product(
+    session: AsyncSession,
+    product: Product,
+) -> None:
+    await session.delete(product)
+    await session.commit()
--- src/users/models.py
+++ src/users/models.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Mapped
 
 
 class Product(Base):
+    __tablename__ = "product"
+
     name: Mapped[str]
     price: Mapped[int]
     description: Mapped[str]
