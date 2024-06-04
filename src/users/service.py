4bd24e82e60a05179ceaf8bdd23257c6bb665d07
ilyamak04 <makilya04@gmail.com>
2024-06-05 00:41:37 +0300

    add new func for communication with the db

--- src/users/service.py
+++ src/users/service.py
@@ -1,6 +1,46 @@
 from src.users.schemas import CreateUserORM
+from src.users.models import *
+from src.database import db_helper
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.orm import joinedload
 
 
 def create_user(user_in: CreateUserORM):
     user = user_in.model_dump()
     return {"success": True, "user": user}
+
+
+async def create_user(session: AsyncSession, username: str) -> User:
+    user = User(username=username)
+    session.add(user)
+    await session.commit()
+    return user
+
+
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    query = select(User).where(User.username == username)
+    result: Result = await session.execute(query)
+    user: User | None = result.scalar_one_or_none()
+    # user: User | None = await session.scalars(query)
+    return user
+
+
+async def show_users_with_profile(session: AsyncSession) -> list[User]:
+    query = select(User).options(joinedload(User.profile)).order_by(User.id)
+    # result: Result = await session.execute()
+    # users = result.scalars()
+    users = await session.scalars(query)
+    return users
+
+
+async def create_posts(
+    session: AsyncSession,
+    user_id: int,
+    *posts_titles: str,
+) -> list[Post]:
+    posts = [Post(title=title, user_id=user_id) for title in posts_titles]
+    session.add_all(posts)
+    await session.commit()
+    return posts
