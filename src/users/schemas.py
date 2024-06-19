67d67ddb0348d11c40fbf1c1fc37a494bbe5f156
ilyamak04 <makilya04@gmail.com>
2024-06-19 10:55:56 +0300

    add user schema

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,8 +1,17 @@
 from annotated_types import MaxLen, MinLen
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 from typing import Annotated
 
 
 class CreateUserORM(BaseModel):
     email: EmailStr
     username: Annotated[str, MaxLen(20), MinLen(3)]
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
