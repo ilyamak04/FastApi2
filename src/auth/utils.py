6af49aa788c215e1c48cad4565251b7aa9658391
ilyamak04 <makilya04@gmail.com>
2024-06-19 10:46:50 +0300

    func for create access refr token

---
+++ src/auth/utils.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+import jwt
+from src.config import settigs
+import bcrypt
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = settigs.auth_jwt.private_key_path.read_text(),
+    algorithm: str = settigs.auth_jwt.algorithms,
+    expire_minutes: int = settigs.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+):
+    to_encode = payload.copy()
+    now = datetime.utcnow()
+    if expire_minutes:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
+    encoded = jwt.encode(
+        to_encode,
+        private_key,
+        algorithm=algorithm,
+    )
+    return encoded
+
+
+def decode_jwt(
+    token: str | bytes,
+    public_key: str = settigs.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settigs.auth_jwt.algorithms,
+):
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithms=[algorithm],
+    )
+    return decoded
+
+
+def hash_password(
+    password: str,
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
